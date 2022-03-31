997b7873d0f529cf1e37a7f033787a43f6c990b6
JosePoPiT <josegarciama23@gmail.com>
2022-03-31 00:28:55 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = '4a05f15d-ad04-4c7c-a648-3c83982a23ec';
+export const MODAL_CLOSE_SEC = 2.5;
